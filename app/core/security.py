7687806bbc02e1935f33470f50f51a3c89feb333
vinayak-jaybhaye <vinayakjaybhaye795@gmail.com>
2026-01-18 11:18:22 +0530

    fixed set_cookie function

--- app/core/security.py
+++ app/core/security.py
@@ -1,20 +1,22 @@
-from datetime import datetime, timedelta, timezone
-from jose import jwt, JWTError
-from passlib.context import CryptContext
-from fastapi import Response
-from typing import Optional
 import hashlib
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+from fastapi import Response
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 from app.core.config import settings
-
 
 pwd_context = CryptContext(
     schemes=["argon2"],
     deprecated="auto",
 )
 
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
@@ -22,10 +24,8 @@ def verify_password(password: str, hashed_password: str) -> bool:
 
 # JWT helpers
 
-def create_access_token(
-    data: dict,
-    expires_minutes: Optional[int] = None
-) -> str:
+
+def create_access_token(data: dict, expires_minutes: Optional[int] = None) -> str:
     expire = datetime.now(timezone.utc) + timedelta(
         minutes=expires_minutes or settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
@@ -38,6 +38,7 @@ def create_access_token(
         settings.JWT_SECRET_KEY,
         algorithm=settings.JWT_ALGORITHM,
     )
+
 
 def decode_access_token(token: str) -> Optional[dict]:
     try:
@@ -52,20 +53,32 @@ def decode_access_token(token: str) -> Optional[dict]:
 
 # Cookie helpers
 
-def set_auth_cookie(response: Response, user) -> None:
-    token = create_access_token(
-        {"sub": user.email, "id": user.id}
-    )
 
+def set_auth_cookie(response: Response, user) -> None:
+    token = create_access_token({"sub": user.email, "id": user.id})
+
+    ### DEV
+    # response.set_cookie(
+    #     key="access_token",
+    #     value=token,
+    #     httponly=True,
+    #     secure=False,
+    #     samesite="lax",
+    #     path="/",
+    #     max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+    # )
+
+    ### PROD
     response.set_cookie(
         key="access_token",
         value=token,
         httponly=True,
-        secure=False,      # True in prod
-        samesite="lax",
+        secure=True,
+        samesite="none",
         path="/",
         max_age=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
     )
+
 
 def delete_auth_cookie(response: Response) -> None:
     response.delete_cookie(
